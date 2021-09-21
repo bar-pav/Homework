3e3a7864b8ad06b20a5c4af51cc6856456b7f585
PavelBarashkin <pavelb7c8@gmail.com>
2021-09-21 22:31:25 +0300

    Remake Task4.6

--- PavelBarashkin/Task4.6.py
+++ PavelBarashkin/Task4.6.py
@@ -4,25 +4,19 @@
 # whitespaces (` `, `\n`, `\t` and so on). If there are multiple longest words in
 # the string with a same length return the word that occures first.
 
-# A character is whitespace if in the Unicode character database (see unicodedata),
-# either its general category is Zs (“Separator, space”), or its bidirectional class is one of WS, B, or S.
-
-import unicodedata
-
 
 def get_longest_word(string):
-    longest_word = ""
-    word = ""
-    string += " "
+    words = []
+    word = ''
     for char in string:
-        if unicodedata.category(char) == "Zs" or (unicodedata.bidirectional(char) in {"WB", "B", "S"}):
-            if len(longest_word) < len(word):
-                longest_word = word
-            word = ""
-            continue
-        word += char
-
-    return longest_word
+        if not char.isspace():
+            word += char
+        elif word:
+            words.append(word)
+            word = ''
+    if word:
+        words.append(word)
+    return max(words, key=len)
 
 
 print(get_longest_word('Python is simple and effective!'))
